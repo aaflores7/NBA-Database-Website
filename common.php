8bd663eb58b6f00edf4874bf5677a60e536b17d7
aaflores7 <139302714+aaflores7@users.noreply.github.com>
2024-06-03 08:54:17 +0800

    Update common.php

--- common.php
+++ common.php
@@ -1,4 +1,7 @@
 <?php 
-  $db = new PDO("mysql:dbname=mynbadb;host=localhost;port=3307", "root", "1234p@55w0rd");
-  $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Edit the database name (mynbadb), host (localhost), port number (3306), username (root), and password (password) as per your database configuration.
+$db = new PDO("mysql:dbname=mynbadb;host=localhost;port=3306", "root", "password");
+
+// Optionally, you can change the error mode attribute. The current setting throws exceptions on errors.
+$db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 ?>
